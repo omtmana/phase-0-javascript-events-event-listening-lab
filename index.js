6b1c36d604bdf419baed3aab0b4f7024056d011d
Olivia <oliviatmanalastas@gmail.com>
2022-06-15 10:53:59 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    alert('I was clicked');
+    input.addEventListener('click', addingEventListener);
 }
+
+input.addEventListener('click', addingEventListener);
+
+
+/*
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+*/
