6eb157a3aa050990939e6070d4fb012c5b5d4af6
Olivia <oliviatmanalastas@gmail.com>
2022-07-05 12:17:13 -0400

    commit tree 51aac91decd55152d571c8e4bfddc3f8174c338a

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const input = document.getElementById('button');
+// const input = document.getElementById('button');
 
-function addingEventListener() {
-    alert('I was clicked');
-    input.addEventListener('click', addingEventListener);
-}
+// function addingEventListener() {
+//     alert('I was clicked');
+//     input.addEventListener('click', addingEventListener);
+// }
 
-input.addEventListener('click', addingEventListener);
-
-
-/*
-const input = document.getElementById('button');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
-*/
+// input.addEventListener('click', addingEventListener);
